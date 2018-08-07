ce9e1da8226d2b8c8226afc9838cdc1ce111d29f
Alex Davidson <alex.davidson@bluewire-technologies.com>
2018-08-07 10:41:21 +0100

    Support newer versions of reports
    Remove hard dependency on 2010 namespaces.
    
    refs E-30974

--- Bluewire.Reporting.Cli/Xml/ReportXmlSchema.cs
+++ Bluewire.Reporting.Cli/Xml/ReportXmlSchema.cs
@@ -1,9 +1,25 @@
-﻿using System.Xml.Linq;
+﻿using System.Text.RegularExpressions;
+using System.Xml.Linq;
 
 namespace Bluewire.Reporting.Cli.Xml
 {
     public class ReportXmlSchema
     {
-        public static readonly XNamespace Xmlns = "http://schemas.microsoft.com/sqlserver/reporting/2010/01/reportdefinition";
+        public static readonly XNamespace Xmlns2010 = "http://schemas.microsoft.com/sqlserver/reporting/2010/01/reportdefinition";
+
+        private static readonly Regex rxReportNamespace = new Regex(@"^http://schemas.microsoft.com/sqlserver/reporting/(?<year>\d+)/(?<month>\d+)/reportdefinition$");
+        public static bool IsReportNamespace(XNamespace xmlns, out int year, out int month)
+        {
+            var m = rxReportNamespace.Match(xmlns.NamespaceName);
+            if (m.Success)
+            {
+                year = int.Parse(m.Groups["year"].Value);
+                month = int.Parse(m.Groups["month"].Value);
+                return true;
+            }
+            year = default(int);
+            month = default(int);
+            return false;
+        }
     }
 }
