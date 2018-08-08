2122809e36fdec9f2e0d019503d29be88f1196a1
Alex Davidson <alex.davidson@bluewire-technologies.com>
2018-08-08 09:56:03 +0100

    Extract common library

--- Bluewire.Reporting.Cli/Support/ConnectionStringUtils.cs
+++ Bluewire.Reporting.Cli/Support/ConnectionStringUtils.cs
@@ -1,21 +1,11 @@
 using System;
 using System.Data.Common;
-using System.Data.SqlClient;
-using System.Net;
 using Bluewire.Common.Console;
 
 namespace Bluewire.Reporting.Cli.Support
 {
     public static class ConnectionStringUtils
     {
-        public static NetworkCredential GetNetworkCredentials(string connectionString)
-        {
-            if (connectionString == null) return null;
-            var builder = new SqlConnectionStringBuilder(connectionString);
-            if (String.IsNullOrWhiteSpace(builder.UserID)) return null;
-            return new NetworkCredential(builder.UserID, builder.Password);
-        }
-
         public static void ValidateArgument<TBuilder>(string connectionString) where TBuilder : DbConnectionStringBuilder, new()
         {
             try
